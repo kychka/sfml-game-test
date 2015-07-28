667aac5b9d2d8f0249a89c9de8a4b9fe7876b1f5
slon <serafim.gerasimov@mail.ru>
2015-07-28 21:15:27 +0400

    slonCircle

--- contrib/main.cpp
+++ contrib/main.cpp
@@ -1,1 +1,24 @@
-// Base code
+#include<SFML\Graphics.hpp>
+#include<iostream>
+#include<ostream>
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(sf::VideoMode(1200, 800), "Circle of slon");
+	CircleShape slonCircle(25.f);
+	slonCircle.setFillColor(Color::Red);
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event)){
+			if (event.type == Event::Closed){ window.close(); }
+		}
+		window.clear();
+		window.draw(slonCircle);
+		window.display();
+	}
+
+	
+	return 0;
+}
