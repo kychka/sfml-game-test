851afea7672d8c5e1c2790aaefe64ccad0fe9d6c
KRaV <antony-krav@yandex.ru>
2015-07-29 23:12:28 +0300

    Create kravTeam.h
    Create files with connection works of all members of krav-team

---
+++ contrib/kravTeam.h
@@ -0,0 +1,25 @@
+/*
+	Test tasks for the team project
+	Circles by krav-team
+	Version 1.0
+	Made by krav-team
+*/
+#ifndef KRAVTEAM_H
+#define KRAVTEAM_H
+
+#include <SFML\Graphics.hpp>
+#include "circleByKrav.h"
+#include "CirclebyLastikPRO.h"
+#include "CircleByRus.h"
+#include "circlebysemitro.h"
+#include "Slon's-Circle.h"
+
+/*
+	function renderCircles()
+
+	Using:
+	- for render circles, use this function, parameter - window for render
+*/
+void renderCircles(sf::RenderWindow &window);
+
+#endif 
