f8b8b4676e082bcfc745ece9e9e0fc7f851590c8
slon <serafim.gerasimov@mail.ru>
2015-07-31 16:00:08 +0400

    новая функция переноса строки по заданному параметру.

--- contrib/lineBreak.cpp
+++ contrib/lineBreak.cpp
@@ -1,15 +1,20 @@
 #include"lineBreak.h"
 
-String lineBreak(String &str1, String &str2, int numberOfCharacters){
+String lineBreak(String &str1, int numberOfCharacters){
 	int strSize = str1.getSize();
 	char ch;
 	if (strSize > numberOfCharacters){
-		str2.clear();
+		String str2="";
 		str2 = str1;
 		ch = str1[numberOfCharacters];
 		str1.erase(numberOfCharacters, strSize - numberOfCharacters);
 		if ((str1[numberOfCharacters-1] != ' ')&&(ch !=' ')){ str1.insert(numberOfCharacters,"-"); }
 		str2.erase(0, numberOfCharacters);
+		str1.insert(str1.getSize(), "\n");
+		if (!str2.isEmpty()){
+			str1.insert(str1.getSize(), lineBreak(str2, numberOfCharacters));
+		}
+
 		
 	}
 	return str1;
--- contrib/lineBreak.h
+++ contrib/lineBreak.h
@@ -5,9 +5,7 @@
 using namespace sf;
 //lineBreak() функция для переноса строки
 //параметры: str1-строка над которой будет производится действие
-//str2-строка в которую будут перенесены символы, которые не поместились в str1
 //numberOfCharacters число символов в str1 после работы функции.
 //функция возвращает измененную str1
-//ВАЖНО: данные находившиеся в str2 до начала работы функции будут удалены.
-String lineBreak(String &str1, String &str2, int numberOfCharacters);
+String lineBreak(String &str1, int numberOfCharacters);
 #endif
