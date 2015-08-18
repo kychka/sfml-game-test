6cc8e5f5adbd10ef948aca03be3753f285f540a7
antony-krav <antony-krav@yandex.ru>
2015-08-18 15:35:58 +0300

    Revert "deleting trash"
    This reverts commit 5682012d1803dbc1602caf9414e2788627b6dba6.

---
+++ simplegroundenemy.cpp
@@ -0,0 +1,40 @@
+#include "simplegroundenemy.h"
+
+SimpleGroundEnemy::SimpleGroundEnemy():Enemy()
+{
+
+}
+
+SimpleGroundEnemy::SimpleGroundEnemy(Vector2f position, String name, int hp, int arm, Animation animation_mass[]) :Enemy(position, name, hp, arm, *animation_mass[])
+{
+    E_DX = NULL;
+    E_DXL = NULL;
+    E_damage = 5;
+    E_HP = hp;
+    E_ARM = arm;
+    E_Speed = 1.0;
+    E_Life = true;
+    E_damage = 15;
+   _onGround = true;
+
+}
+SimpleGroundEnemy::~SimpleGroundEnemy()
+{
+
+}
+void SimpleGroundEnemy::collision(std::vector<lv::Object> &allObj)
+{
+     Enemy::collision(&allObj);
+}
+int SimpleGroundEnemy::getDamage()
+{
+     Enemy::getDamage();
+}
+bool SimpleGroundEnemy::isAlive()
+{
+     Enemy::isAlive();
+}
+void SimpleGroundEnemy::setDamage(int damage)
+{
+    Enemy::setDamage(damage);
+}
