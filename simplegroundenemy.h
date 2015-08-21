ca6f77c97a8bdb8d6975c89c51da27f2ae93ff26
slon <serafim.gerasimov@mail.ru>
2015-08-21 19:54:24 +0400

    no message

---
+++ simplegroundenemy.h
@@ -0,0 +1,19 @@
+﻿#ifndef SIMPLEGROUNDENEMY_H
+#define SIMPLEGROUNDENEMY_H
+
+#include "HpBar/hp_bar.h"
+#include "Enemy.h"
+
+class SimpleGroundEnemy :public Enemy
+{
+public:
+	SimpleGroundEnemy();
+	SimpleGroundEnemy(Vector2f position, String name, int hp, int arm, Animation animation_mass[]);
+	~SimpleGroundEnemy();
+	void collision(std::vector<lv::Object> &allObj);
+	bool isAlive();
+	void setDamage(int damage);
+	int getDamage();
+};
+
+#endif // SIMPLEGROUNDENEMY_H
